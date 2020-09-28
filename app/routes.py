87a544e741d577b52585a1f16404ff31a18d75ee
Zheng Wang <zheng.wang@usequantum.com>
2020-09-27 17:05:13 -0700

    add first image

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 from flask import render_template, flash, redirect
-from flask_login import current_user, login_user
+from flask_login import current_user, login_user, logout_user
 from app import app, forms
 from app.models import User
+from datetime import timedelta
 
 
 @app.route('/')
@@ -19,11 +20,26 @@ def login():
 		if user is None or not user.check_password(form.password.data):
 			flash('Invalid username or password')
 			return redirect('/login')
-		login_user(user)
+		login_user(user, duration=timedelta(days=0, seconds=3, microseconds=0, milliseconds=0, minutes=0, hours=0, weeks=0))
 		return redirect('/chat')
 	return render_template('login.html', title='Login', form=form)
 
 
+@app.route('/logout')
+def logout():
+	logout_user()
+	return redirect('/')
+
+
 @app.route('/chat')
 def chat():
-	return render_template('chat.html')
+	if current_user.is_authenticated:
+		return render_template('chat.html')
+	return redirect('/')
+
+
+@app.route('/gallery')
+def gallery():
+	if current_user.is_authenticated:
+		return render_template('gallery.html')
+	return redirect('/')
