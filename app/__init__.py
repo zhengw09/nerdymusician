7df8c8099ca7d0c585e0b394324a98eca4731d89
Zheng Wang <zheng.wang@usequantum.com>
2020-11-13 06:45:43 -0800

    test

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,6 @@ app.secret_key = 'ThisWillBeOurYear'
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 login = LoginManager(app)
-app.permanent_session_lifetime = timedelta(seconds=170)
+app.permanent_session_lifetime = timedelta(seconds=25)
 
 from app import routes, models
