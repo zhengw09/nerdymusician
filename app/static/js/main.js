24f39ff50b86ad8b5a70aa56646152d6b252dff1
Zheng Wang <zheng.wang@usequantum.com>
2020-11-18 19:33:09 -0800

    self logout

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,6 +1,6 @@
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        window.location.href = "http://nerdymusician.com";
+
     } else {
         window.location.href = "http://nerdymusician.com";
     }
