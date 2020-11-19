31f5205946efebed049fd6ee53748c06cc7ab5fa
Zheng Wang <zheng.wang@usequantum.com>
2020-11-18 21:46:38 -0800

    hidden

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,6 +1,6 @@
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
         window.location.href = "http://nerdymusician.com";
-        document.getElementById("demo").innerHTML = "show";
+        document.getElementById("demo").innerHTML = "hidden";
     }
 });
