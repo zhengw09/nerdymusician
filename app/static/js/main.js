96a4275fe669d2f67f03ce862e7148a656cc15e1
Zheng Wang <zheng.wang@usequantum.com>
2020-12-09 13:58:42 -0800

    parse links

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -3,3 +3,7 @@ document.addEventListener('visibilitychange', function () {
         window.location.href = "http://nerdymusician.com";
     }
 });
+
+var textNode = document.getElementById("msgs");
+console.log(textNode);
+textNode.innerHTML = textNode.innerHTML.replace(/(https?:\/\/[^\s]+)/g, "<a href='$1'>$1</a>");
